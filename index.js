dc6d7a84d3083c5304106f6011b72c2725f81175
tatiana.mojica <tatiana.mojica@bustle.com>
2019-10-07 23:42:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+// document.body.appendChild(h1#victory);
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Tatiana is the champion"
